4462be9296a3cb7b608da4c7173a130e8ec79bac
Andrea Campi <andrea.campi@zephirworks.com>
2012-04-06 10:35:14 +0200

    Bump version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Rails applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.99.12"
+version          "0.99.14"
 
 %w{ application runit unicorn apache2 passenger_apache2 }.each do |cb|
   depends cb
