6b8c5e4fa3f9f9694781a6935ee3082d5aa6c259
Andrea Campi <andrea.campi@zephirworks.com>
2012-04-20 12:58:15 +0200

    Update the description.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
-description      "Deploys and configures Rails applications"
+description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.99.14"
 
