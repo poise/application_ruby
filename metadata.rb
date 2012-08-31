b0e9fb7c9befe86404015ca8276f05f0f88dc80f
jtimberman <joshua@opscode.com>
2012-08-31 10:13:50 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 %w{ application runit unicorn apache2 passenger_apache2 }.each do |cb|
   depends cb
