3c2bef815fb13adc768456029a3bc51883c7a116
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-11-27 17:58:14 +0000

    Version bump, 1.0.7

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.6"
+version          "1.0.7"
 
 %w{ application runit unicorn apache2 passenger_apache2 }.each do |cb|
   depends cb
