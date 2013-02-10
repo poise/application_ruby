a9921483bcef958608bcac013dc97a8778c51d1d
jtimberman <joshua@opscode.com>
2013-02-09 22:36:24 -0700

    [COOK-2363] run_restart no longer required

--- providers/unicorn.rb
+++ providers/unicorn.rb
@@ -78,7 +78,6 @@ action :before_restart do
       :rails_env => new_resource.environment_name,
       :smells_like_rack => ::File.exists?(::File.join(new_resource.path, "current", "config.ru"))
     )
-    run_restart false
   end
 
 end
