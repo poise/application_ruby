56246a8c4d0d212dbdcfc13786ec36bdbdc34395
Steve Meyfroidt <steve.meyfroidt@gmail.com>
2013-03-25 13:26:17 +0000

    Allow a unicorn env to be defined

--- resources/unicorn.rb
+++ resources/unicorn.rb
@@ -27,6 +27,7 @@ attribute :port, :kind_of => String, :default => "8080"
 attribute :worker_timeout, :kind_of => Integer, :default => 60
 attribute :bundler, :kind_of => [TrueClass, FalseClass, NilClass], :default => nil
 attribute :bundle_command, :kind_of => [String, NilClass], :default => nil
+attribute :environment, :kind_of => Hash, :default => {}
 
 def options(*args, &block)
   @options ||= Mash[:tcp_nodelay => true, :backlog => 100]
