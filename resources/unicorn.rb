c58da193eda9037e09e47facbffbc5a555ab919d
Steve Meyfroidt <steve.meyfroidt@gmail.com>
2013-07-22 11:58:02 +0100

    Add before_exec to resource def

--- resources/unicorn.rb
+++ resources/unicorn.rb
@@ -24,6 +24,7 @@ attribute :preload_app, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :worker_processes, :kind_of => Integer, :default => [node['cpu']['total'].to_i * 4, 8].min
 attribute :before_fork, :kind_of => String, :default => 'sleep 1'
 attribute :after_fork, :kind_of => String, :default => nil
+attribute :before_exec, :kind_of => String, :default => nil
 attribute :port, :kind_of => String, :default => "8080"
 attribute :worker_timeout, :kind_of => Integer, :default => 60
 attribute :bundler, :kind_of => [TrueClass, FalseClass, NilClass], :default => nil
