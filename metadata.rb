c0e59b05e6265f1deef316fcd7db81a75c91a26c
Josh Murphy <josh.murphy@cerner.com>
2014-06-18 12:55:37 -0500

    Bump version to 3.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.2"
+version          "3.0.3"
 
 %w{ unicorn apache2 passenger_apache2 }.each do |cb|
   depends cb
