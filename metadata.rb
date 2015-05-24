8ee6bf3b79b7e406ff0af090774c80784261cde9
Roland Moriz <roland@moriz.de>
2015-05-24 15:18:38 +0200

    add source_url attribute for chef supermarket

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
+source_url       "https://github.com/poise/application_ruby"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "3.0.2"
 
