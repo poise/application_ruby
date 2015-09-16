44096bb472d82aa34947edfdae4cab20432ae74c
Noah Kantrowitz <noah@coderanger.net>
2015-09-16 12:49:45 -0700

    Improvements to the ruby_execute.
    Now it pulls in user, group, and environment variables from the app.

--- lib/poise_application_ruby/resources/ruby_execute.rb
+++ lib/poise_application_ruby/resources/ruby_execute.rb
@@ -36,12 +36,52 @@ module PoiseApplicationRuby
       class Resource < PoiseRuby::Resources::RubyExecute::Resource
         include PoiseApplicationRuby::AppMixin
         provides(:application_ruby_ruby_execute)
-        subclass_providers!
+
+        def initialize(*args)
+          super
+          # Clear some instance variables so my defaults work.
+          remove_instance_variable(:@cwd)
+          remove_instance_variable(:@group)
+          remove_instance_variable(:@user)
+        end
 
         # #!attribute cwd
         #   Override the default directory to be the app path if unspecified.
         #   @return [String]
         attribute(:cwd, kind_of: [String, NilClass, FalseClass], default: lazy { parent && parent.path })
+
+        # #!attribute group
+        #   Override the default group to be the app group if unspecified.
+        #   @return [String, Integer]
+        attribute(:group, kind_of: [String, Integer, NilClass, FalseClass], default: lazy { parent && parent.group })
+
+        # #!attribute user
+        #   Override the default user to be the app owner if unspecified.
+        #   @return [String, Integer]
+        attribute(:user, kind_of: [String, Integer, NilClass, FalseClass], default: lazy { parent && parent.owner })
+      end
+
+      # The default provider for `application_ruby_ruby_execute`.
+      #
+      # @see Resource
+      # @provides application_ruby_ruby_execute
+      class Provider < PoiseRuby::Resources::RubyExecute::Provider
+        provides(:application_ruby_ruby_execute)
+
+        private
+
+        # Override environment to add the application envivonrment instead.
+        #
+        # @return [Hash]
+        def environment
+          super.tap do |environment|
+            # Don't use the app_state_environment_ruby because we already have
+            # those values in place.
+            environment.update(new_resource.app_state_environment)
+            # Re-apply the resource environment for correct ordering.
+            environment.update(new_resource.environment) if new_resource.environment
+          end
+        end
       end
 
     end
---
+++ test/spec/resources/ruby_execute_spec.rb
@@ -0,0 +1,46 @@
+#
+# Copyright 2015, Noah Kantrowitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe PoiseApplicationRuby::Resources::RubyExecute do
+  step_into(:application_ruby_ruby_execute)
+  recipe do
+    application '/srv/myapp' do
+      owner 'myuser'
+      group 'mygroup'
+      environment ENVKEY: 'ENVVALUE'
+
+      ruby('') { provider :dummy }
+      ruby_execute 'myapp.rb'
+    end
+  end
+
+  it do
+    expect_any_instance_of(described_class::Provider).to receive(:shell_out!).with(
+      '/ruby myapp.rb',
+      user: 'myuser',
+      group: 'mygroup',
+      cwd: '/srv/myapp',
+      timeout: 3600,
+      returns: 0,
+      environment: {'ENVKEY' => 'ENVVALUE'},
+      log_level: :info,
+      log_tag: 'application_ruby_ruby_execute[myapp.rb]',
+    )
+    is_expected.to run_application_ruby_ruby_execute('myapp.rb').with(user: 'myuser', group: 'mygroup', cwd: '/srv/myapp')
+  end
+end
