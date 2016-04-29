d41ff993211537c85f628e44e884946371cddd11
Noah Kantrowitz <noah@coderanger.net>
2016-04-29 00:17:23 -0700

    Trying to fix an issue with Chef master where Chef's embedded bin folder is ending up on $PATH and it's commands are taking priority over the one's from the gem install.
    /cc @lamont-granquist in case you have any input.

--- lib/poise_application_ruby/service_mixin.rb
+++ lib/poise_application_ruby/service_mixin.rb
@@ -39,6 +39,7 @@ module PoiseApplicationRuby
     module Provider
       include PoiseApplication::ServiceMixin::Provider
       include PoiseApplicationRuby::AppMixin::Provider
+      include PoiseRuby::RubyCommandMixin::Provider
       include PoiseRuby::BundlerMixin
 
       # Set up the service for running Ruby stuff.
@@ -48,10 +49,16 @@ module PoiseApplicationRuby
         self_ = self
         # Create a new singleton method that fills in Python for you.
         resource.define_singleton_method(:ruby_command) do |val|
-          path = self_.new_resource.app_state_environment_ruby['PATH']
+          path = self_.new_resource.app_state_environment_ruby['PATH'] || ENV['PATH']
           cmd = if self_.new_resource.parent_bundle
             bundle_exec_command(val, path: path)
           else
+            # Insert the gem executable directory at the front of the path.
+            gem_environment = self_.send(:ruby_shell_out!, self_.new_resource.gem_binary, 'environment')
+            matches = gem_environment.stdout.scan(/EXECUTABLE DIRECTORY: (.*)$/).first
+            if matches
+              path = "#{matches.first}:#{path}"
+            end
             "#{self_.new_resource.ruby} #{PoiseLanguages::Utils.absolute_command(val, path: path)}"
           end
           resource.command(cmd)
